27b96e1265d4973f011d61cc65404c1d4c700faf
Nahuel-Ranz <elhacha_nahuel@hotmail.com>
2025-05-09 17:02:49 -0300

    cambio de index

--- reserva.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE reservas (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(100),
-  servicio VARCHAR(100),
-  fecha DATE,
-  hora TIME,
-  email VARCHAR(100),
-  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
