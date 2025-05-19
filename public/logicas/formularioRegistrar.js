9ec5dd1ec3f37fd6d396fe969d35fdf3d95ed6ee
Nahuel-Ranz <elhacha_nahuel@hotmail.com>
2025-05-19 20:59:33 -0300

    cambio de servidor a ejs

---
+++ public/logicas/formularioRegistrar.js
@@ -0,0 +1,90 @@
+const formulario=document.getElementById('formularioRegistro');
+const inputs=document.querySelectorAll('input');
+const reset=document.getElementById('reset');
+const verificarDatos=document.getElementById('verificarDatos');
+const registrar=document.getElementById('registrar');
+
+const regExpresion = {
+    nombre: /^[a-zA-ZÀ-ÿ\s]{3,25}$/i,
+    apellido: /^[a-zA-ZÀ-ÿ\s]{3,25}$/i,
+    dni: /^\d{8}$/,
+    celular: /^\d{10}$/,
+    correo: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i,
+    contrasenia: /^.{8,20}$/
+}
+
+const estadoInputs = {
+    nombre: false,
+    apellido: false,
+    dni: false,
+    celular: false,
+    correo: false,
+    contrasenia: false
+}
+
+reset.addEventListener('click', () => {
+    const grupos = ['Nombre', 'Apellido', 'Dni', 'Celular', 'Correo', 'Contrasenia', 'Contrasenia2'];
+
+    grupos.forEach(element => {
+        document.getElementById(`grupo${element}`).classList.remove('correcto');
+        document.getElementById(`grupo${element}`).classList.remove('incorrecto');
+        estadoInputs[`${element.toLowerCase()}`]=false;
+    });
+});
+
+verificarDatos.addEventListener('click', () => {
+    let bool=false;
+
+    for(let estado in estadoInputs) {
+        if(!estadoInputs[estado]) { bool=true; break;}
+    }
+
+    if(bool) alert('Complete todos los campos del formulario con datos correctos, por favor!');
+});
+
+const regularExpresion = (elemento, expresionReg, grupo) => {
+    if(expresionReg.test(elemento.value)) {
+        document.getElementById(`grupo${grupo}`).classList.remove('incorrecto');
+        document.getElementById(`grupo${grupo}`).classList.add('correcto');
+        estadoInputs[grupo.toLowerCase()] = true;
+    } else {
+        document.getElementById(`grupo${grupo}`).classList.remove('correcto');
+        document.getElementById(`grupo${grupo}`).classList.add('incorrecto');
+        estadoInputs[grupo.toLowerCase()] = false;
+    }
+};
+
+const verificarContrasenias = () => {
+    const pass = document.getElementById('contrasenia');
+    const pass2 = document.getElementById('contrasenia2');
+
+    if(pass.value !== pass2.value) {
+        document.getElementById('grupoContrasenia2').classList.remove('correcto');
+        document.getElementById('grupoContrasenia2').classList.add('incorrecto');
+        estadoInputs.contrasenia = false;
+    } else {
+        document.getElementById('grupoContrasenia2').classList.remove('incorrecto');
+        document.getElementById('grupoContrasenia2').classList.add('correcto');
+        estadoInputs.contrasenia = true;
+    }
+};
+
+const verificarEntrada = (e) => {
+    switch(e.target.id) {
+        case 'nombre': regularExpresion(e.target, regExpresion.nombre, 'Nombre'); break;
+        case 'apellido': regularExpresion(e.target, regExpresion.apellido, 'Apellido'); break;
+        case 'dni': regularExpresion(e.target, regExpresion.dni, 'Dni'); break;
+        case 'celular': regularExpresion(e.target, regExpresion.celular, 'Celular'); break;
+        case 'correo': regularExpresion(e.target, regExpresion.correo, 'Correo'); break;
+        case 'contrasenia':
+            regularExpresion(e.target, regExpresion.contrasenia, 'Contrasenia');
+            verificarContrasenias();
+            break;
+        case 'contrasenia2': verificarContrasenias();
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', verificarEntrada);
+    input.addEventListener('blur', verificarEntrada);
+});
