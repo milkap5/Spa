13a5f01e47f48005736b56d02fd0686f22266a13
Nahuel-Ranz <elhacha_nahuel@hotmail.com>
2025-06-13 16:35:37 -0300

    ..

--- app.js
+++ app.js
@@ -34,3 +34,8 @@ app.use((req, res) => {
 app.listen(3000, () => {
     console.log('Servidor creado: http://localhost:3000');
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor en puerto ${PORT}`);
+});
